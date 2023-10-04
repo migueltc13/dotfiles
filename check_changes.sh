f36b85fa64edacda009cf7157856eb3e80c9defd
migueltc13 <migueltc13@gmail.com>
2023-10-04 17:34:13 +0100

    Update check_changes.sh

--- check_changes.sh
+++ check_changes.sh
@@ -10,16 +10,29 @@ fi
 notify () {
   diff_status=$?
   if (( $diff_status != 0 )); then
-    echo "Changes in $2"
+
+    # TODO: add header to less command
+    GIT_PAGER="less -FRX" $diff --color=always $1 $2 | $less -R
+
+    echo "cp -r $2 $1"
+    read -p "Do you want to copy these changes? [y/N] " choice
+    if [[ "$choice" =~ [yY] ]]; then
+      echo "Copying changes..."
+      cp -r $2 $1
+    else
+      echo "Changes were not copied."
+    fi
+
     ((count++))
   fi
 }
 
+# TODO change to files fullpath instead
 check () {
-  PATH="$1"
-  FILE="$2"
-  $diff $PATH$FILE $FILE 1>/dev/null
-  notify $PATH$FILE $FILE
+  FILE1="$1"
+  FILE2="$2"
+  $diff $FILE1 $FILE2 1>/dev/null
+  notify $FILE1 $FILE2
 }
 
 count=0
@@ -30,19 +43,21 @@ cut=$(which cut)
 apt=$(which apt)
 snap=$(which snap)
 tail=$(which tail)
+less=$(which less)
 
-check "$HOME/" ".bash_aliases"
-check "/" "usr/lib/command-not-found"
-check "/" "usr/local/bin"
-check "$HOME/" ".local/share/gnome-shell/extensions/"
+check "$HOME/.bashrc" ".bashrc"
+check "$HOME/.bash_aliases" ".bash_aliases"
+check "/usr/lib/command-not-found" "usr/lib/command-not-found"
+check "/usr/local/bin" "usr/local/bin"
+check "$HOME/.local/share/gnome-shell/extensions/" ".local/share/gnome-shell/extensions/"
 
 # apt-packages.txt
 $apt list --installed 2>/dev/null | $grep '\[installed\]' | $cut -d'/' -f1 1> /tmp/apt-packages.txt
-check "/tmp/" "apt-packages.txt"
+check "apt-packages.txt" "/tmp/apt-packages.txt"
 
 # snap-packages.txt
 $snap list | $tail -n +2 | $cut -d' ' -f1 1> /tmp/snap-packages.txt
-check "/tmp/" "snap-packages.txt"
+check "snap-packages.txt" "/tmp/snap-packages.txt"
 
 # TODO: /opt dir
 # TODO: ~/Git dir
