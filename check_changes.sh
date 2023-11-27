3816b4a0b80e27c055a3bee5408db6a05f0c8d48
migueltc13 <migueltc13@gmail.com>
2023-11-27 13:43:55 +0000

    Add  to check_changes.sh

--- check_changes.sh
+++ check_changes.sh
@@ -92,6 +92,8 @@ check "/tmp/snap-packages.txt" "snap-packages.txt"
 # TODO: More config files: like .Xresources, .nanorc, ...
 # TODO: apps
 
+sudo chown -R $USER .
+
 if (( $changes_count == 0 )); then
   echo "No changes detected"
 fi
