3207b5b5993b2995419d43b1166f9b3fffc21aab
migueltc13 <migueltc13@gmail.com>
2023-12-05 16:49:10 +0000

    Add .fzf.bash

---
+++ .fzf.bash
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/z0d1ac/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/z0d1ac/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+source "/home/z0d1ac/.fzf/shell/completion.bash"
+
+# Key bindings
+# ------------
+source "/home/z0d1ac/.fzf/shell/key-bindings.bash"
+
+# Env variables
+# -------------
+export FZF_DEFAULT_OPTS="--layout=reverse --ansi --color=dark"
+export FZF_CTRL_R_OPTS="--prompt '$ '"
+# export FZF_DEFAULT_COMMAND='find .'
+export FZF_ALT_C_COMMAND='find . -type d'
