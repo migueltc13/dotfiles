12f09b2c6d8c073d8702de1500df78743a6f0654
migueltc13 <migueltc13@gmail.com>
2023-12-14 03:19:00 +0000

    Clean util dir, create ui for nvim-notify and copilot notifications

--- .config/nvim/lua/util/telescope.lua
+++ .config/nvim/lua/util/telescope.lua
@@ -7,9 +7,9 @@ local Util = require("util")
 ---@class util.telescope
 ---@overload fun(builtin:string, opts?:util.telescope.opts)
 local M = setmetatable({}, {
-  __call = function(m, ...)
-    return m.telescope(...)
-  end,
+    __call = function(m, ...)
+        return m.telescope(...)
+    end,
 })
 
 -- this will return a function that calls telescope.
@@ -18,40 +18,40 @@ local M = setmetatable({}, {
 ---@param builtin string
 ---@param opts? util.telescope.opts
 function M.telescope(builtin, opts)
-  local params = { builtin = builtin, opts = opts }
-  return function()
-    builtin = params.builtin
-    opts = params.opts
-    opts = vim.tbl_deep_extend("force", { cwd = Util.root() }, opts or {}) --[[@as util.telescope.opts]]
-    if builtin == "files" then
-      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
-        opts.show_untracked = true
-        builtin = "git_files"
-      else
-        builtin = "find_files"
-      end
-    end
-    if opts.cwd and opts.cwd ~= vim.loop.cwd() then
-      ---@diagnostic disable-next-line: inject-field
-      opts.attach_mappings = function(_, map)
-        map("i", "<a-c>", function()
-          local action_state = require("telescope.actions.state")
-          local line = action_state.get_current_line()
-          M.telescope(
-            params.builtin,
-            vim.tbl_deep_extend("force", {}, params.opts or {}, { cwd = false, default_text = line })
-          )()
-        end)
-        return true
-      end
-    end
+    local params = { builtin = builtin, opts = opts }
+    return function()
+        builtin = params.builtin
+        opts = params.opts
+        if builtin == "files" then
+            ---@diagnostic disable-next-line: undefined-field
+            if vim.loop.fs_stat((opts and opts.cwd or vim.loop.cwd()) .. "/.git") then
+                opts.show_untracked = true
+                builtin = "git_files"
+            else
+                builtin = "find_files"
+            end
+        end
+        if opts and opts.cwd and opts.cwd ~= vim.loop.cwd() then
+            ---@diagnostic disable-next-line: inject-field
+            opts.attach_mappings = function(_, map)
+                map("i", "<a-c>", function()
+                    local action_state = require("telescope.actions.state")
+                    local line = action_state.get_current_line()
+                    M.telescope(
+                        params.builtin,
+                        vim.tbl_deep_extend("force", {}, params.opts or {}, { cwd = false, default_text = line })
+                    )()
+                end)
+                return true
+            end
+        end
 
-    require("telescope.builtin")[builtin](opts)
-  end
+        require("telescope.builtin")[builtin](opts)
+    end
 end
 
 function M.config_files()
-  return Util.telescope("find_files", { cwd = vim.fn.stdpath("config") })
+    return Util.telescope("find_files", { cwd = vim.fn.stdpath("config") })
 end
 
 return M
--- .config/nvim/lua/util/ui.lua
+++ .config/nvim/lua/util/ui.lua
@@ -1,182 +1,74 @@
+---@class util.ui.opts
+
 ---@class util.ui
-local M = {}
+---@overload fun(builtin:string, opts?:util.ui.opts)
+local M = setmetatable({}, {
+    __call = function(m, ...)
+        return m.ui(...)
+    end,
+})
 
----@alias Sign {name:string, text:string, texthl:string, priority:number}
+-- nvim-notify
 
--- Returns a list of regular and extmark signs sorted by priority (low to high)
----@return Sign[]
----@param buf number
----@param lnum number
-function M.get_signs(buf, lnum)
-  -- Get regular signs
-  ---@type Sign[]
-  local signs = vim.tbl_map(function(sign)
-    ---@type Sign
-    local ret = vim.fn.sign_getdefined(sign.name)[1]
-    ret.priority = sign.priority
-    return ret
-  end, vim.fn.sign_getplaced(buf, { group = "*", lnum = lnum })[1].signs)
+M.notify_icons = {
+    on = "🌣",
+    off = "☾",
+}
 
-  -- Get extmark signs
-  local extmarks = vim.api.nvim_buf_get_extmarks(
-    buf,
-    -1,
-    { lnum - 1, 0 },
-    { lnum - 1, -1 },
-    { details = true, type = "sign" }
-  )
-  for _, extmark in pairs(extmarks) do
-    signs[#signs + 1] = {
-      name = extmark[4].sign_hl_group or "",
-      text = extmark[4].sign_text,
-      texthl = extmark[4].sign_hl_group,
-      priority = extmark[4].priority,
-    }
-  end
-
-  -- Sort by priority
-  table.sort(signs, function(a, b)
-    return (a.priority or 0) < (b.priority or 0)
-  end)
-
-  return signs
+-- Used in ../plugins/ui/nvim-notify.lua
+function M.init_status_notify()
+    vim.g.status_notify = true
 end
 
----@return Sign?
----@param buf number
----@param lnum number
-function M.get_mark(buf, lnum)
-  local marks = vim.fn.getmarklist(buf)
-  vim.list_extend(marks, vim.fn.getmarklist())
-  for _, mark in ipairs(marks) do
-    if mark.pos[1] == buf and mark.pos[2] == lnum and mark.mark:match("[a-zA-Z]") then
-      return { text = mark.mark:sub(2), texthl = "DiagnosticHint" }
-    end
-  end
+-- Used in ../plugins/ui/lualine.lua
+function M.cond_status_notify()
+    return vim.g.status_notify == false
 end
 
----@param sign? Sign
----@param len? number
-function M.icon(sign, len)
-  sign = sign or {}
-  len = len or 2
-  local text = vim.fn.strcharpart(sign.text or "", 0, len) ---@type string
-  text = text .. string.rep(" ", len - vim.fn.strchars(text))
-  return sign.texthl and ("%#" .. sign.texthl .. "#" .. text .. "%*") or text
-end
-
-function M.foldtext()
-  local ok = pcall(vim.treesitter.get_parser, vim.api.nvim_get_current_buf())
-  local ret = ok and vim.treesitter.foldtext and vim.treesitter.foldtext()
-  if not ret or type(ret) == "string" then
-    ret = { { vim.api.nvim_buf_get_lines(0, vim.v.lnum - 1, vim.v.lnum, false)[1], {} } }
-  end
-  table.insert(ret, { " " .. require("lazyvim.config").icons.misc.dots })
-
-  if not vim.treesitter.foldtext then
-    return table.concat(
-      vim.tbl_map(function(line)
-        return line[1]
-      end, ret),
-      " "
-    )
-  end
-  return ret
-end
-
-function M.statuscolumn()
-  local win = vim.g.statusline_winid
-  local buf = vim.api.nvim_win_get_buf(win)
-  local is_file = vim.bo[buf].buftype == ""
-  local show_signs = vim.wo[win].signcolumn ~= "no"
-
-  local components = { "", "", "" } -- left, middle, right
-
-  if show_signs then
-    ---@type Sign?,Sign?,Sign?
-    local left, right, fold
-    for _, s in ipairs(M.get_signs(buf, vim.v.lnum)) do
-      if s.name and s.name:find("GitSign") then
-        right = s
-      else
-        left = s
-      end
-    end
-    if vim.v.virtnum ~= 0 then
-      left = nil
-    end
-    vim.api.nvim_win_call(win, function()
-      if vim.fn.foldclosed(vim.v.lnum) >= 0 then
-        fold = { text = vim.opt.fillchars:get().foldclose or "", texthl = "Folded" }
-      end
-    end)
-    -- Left: mark or non-git sign
-    components[1] = M.icon(M.get_mark(buf, vim.v.lnum) or left)
-    -- Right: fold icon or git sign (only if file)
-    components[3] = is_file and M.icon(fold or right) or ""
-  end
-
-  -- Numbers in Neovim are weird
-  -- They show when either number or relativenumber is true
-  local is_num = vim.wo[win].number
-  local is_relnum = vim.wo[win].relativenumber
-  if (is_num or is_relnum) and vim.v.virtnum == 0 then
-    if vim.v.relnum == 0 then
-      components[2] = is_num and "%l" or "%r" -- the current line
+-- Used in ../core/keymaps.lua
+function M.toggle_notify()
+    if vim.g.status_notify == true then
+        vim.g.status_notify = false
+        require("notify").dismiss()
+        vim.notify("Notifications disabled", 3, { title = "nvim-notify", icon = M.notify_icons.off })
     else
-      components[2] = is_relnum and "%r" or "%l" -- other lines
+        vim.g.status_notify = true
+        -- setup using ../plugins/ui/nvim-notify.lua
+        require("plugins.ui.nvim-notify").config()
+        vim.notify("Notifications enabled", 3, { title = "nvim-notify", icon = M.notify_icons.on })
     end
-    components[2] = "%=" .. components[2] .. " " -- right align
-  end
-
-  return table.concat(components, "")
 end
 
-function M.fg(name)
-  ---@type {foreground?:number}?
-  ---@diagnostic disable-next-line: deprecated
-  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
-  ---@diagnostic disable-next-line: undefined-field
-  local fg = hl and (hl.fg or hl.foreground)
-  return fg and { fg = string.format("#%06x", fg) } or nil
+-- Used in ../plugins/ui/lualine.lua
+function M.curr_notify_icon()
+    if vim.g.status_notify == true then
+        return M.notify_icons.on
+    else
+        return M.notify_icons.off
+    end
 end
 
-M.skip_foldexpr = {} ---@type table<number,boolean>
-local skip_check = assert(vim.loop.new_check())
+-- copilot
 
-function M.foldexpr()
-  local buf = vim.api.nvim_get_current_buf()
+M.copilot_icons = {
+    on = "🛩️",
+    off = "🚫"
+}
 
-  -- still in the same tick and no parser
-  if M.skip_foldexpr[buf] then
-    return "0"
-  end
+-- Used in ../plugins/copilot.lua
+function M.init_copilot()
+    vim.g.copilot = true
+end
 
-  -- don't use treesitter folds for non-file buffers
-  if vim.bo[buf].buftype ~= "" then
-    return "0"
-  end
-
-  -- as long as we don't have a filetype, don't bother
-  -- checking if treesitter is available (it won't)
-  if vim.bo[buf].filetype == "" then
-    return "0"
-  end
-
-  local ok = pcall(vim.treesitter.get_parser, buf)
-
-  if ok then
-    return vim.treesitter.foldexpr()
-  end
-
-  -- no parser available, so mark it as skip
-  -- in the next tick, all skip marks will be reset
-  M.skip_foldexpr[buf] = true
-  skip_check:start(function()
-    M.skip_foldexpr = {}
-    skip_check:stop()
-  end)
-  return "0"
+-- Used in ../core/keymaps.lua
+function M.toggle_copilot()
+    if vim.g.copilot == true then
+        vim.g.copilot = false
+        vim.notify("Copilot disabled", 3, { title = "copilot", icon = M.copilot_icons.off })
+    else
+        vim.g.copilot = true
+        vim.notify("Copilot enabled", 3, { title = "copilot", icon = M.copilot_icons.on })
+    end
 end
 
 return M
