c3a166813363cd40618336857feacd63883d46d6
migueltc13 <migueltc13@gmail.com>
2024-01-01 21:45:05 +0000

    Rename lspconfig.lua -> nvim-lspconfig.lua + removed nvim-lsp-file-operations plugin

--- .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -2,8 +2,8 @@ return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        -- "hrsh7th/cmp-nvim-lsp",
-        { "antosha417/nvim-lsp-file-operations", config = true },
+        "mason.nvim",
+        -- { "antosha417/nvim-lsp-file-operations", config = true },
     },
     config = function()
         -- import lspconfig plugin
