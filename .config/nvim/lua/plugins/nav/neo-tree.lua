c454b3021e81a3db2289c20751b085a596f85884
migueltc13 <migueltc13@gmail.com>
2024-01-01 21:49:00 +0000

    Lazy load neo-tree with init function

--- .config/nvim/lua/plugins/nav/neo-tree.lua
+++ .config/nvim/lua/plugins/nav/neo-tree.lua
@@ -1,16 +1,23 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    -- cmd = "Neotree",
-    -- event = "VeryLazy",
     branch = "v3.x",
+    cmd = "Neotree",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Image support in preview window: See `# Preview Mode` for more information
     },
+    init = function()
+        if vim.fn.argc(-1) == 1 then
+            local stat = vim.loop.fs_stat(vim.fn.argv(0))
+            if stat and stat.type == "directory" then
+                require("neo-tree")
+            end
+        end
+    end,
     deactivate = function()
-        vim.cmd([[Neotree close]])
+        vim.cmd("Neotree close")
     end,
     opts = {
         sources = { "filesystem", "buffers", "git_status", "document_symbols" },
@@ -21,7 +28,7 @@ return {
             use_libuv_file_watcher = true,
             filtered_items = {
                 visible = true,
-                show_hidden_count = true,
+                show_hidden_count = false,
                 hide_dotfiles = false,
                 hide_gitignored = false,
                 hide_by_name = {
@@ -36,24 +43,8 @@ return {
             mappings = {
                 ["<space>"] = "none",
                 ["O"] = {
-                    command = function(state)
-                        local node = state.tree:get_node()
-                        local filepath = node.path
-                        -- local osType = os.getenv("OS")
-
-                        local command = "xdg-open '" .. filepath .. "'"
-                        vim.cmd("!" .. command)
-
-                        -- if osType == "Windows_NT" then
-                        --     command = "start " .. filepath
-                        -- elseif osType == "Darwin" then
-                        --     command = "open " .. filepath
-                        -- else
-                        --     command = "xdg-open " .. filepath
-                        -- end
-                        -- os.execute(command)
-                    end,
-                    desc = "open_with_system_defaults",
+                    command = require("util.NeoTree").open_in_system,
+                    desc = "open_with_system_default",
                 },
             },
         },
@@ -66,26 +57,4 @@ return {
             },
         },
     },
-    -- config = function(_, opts)
-    --     -- TODO: Implement this lsp.on_rename function
-    --     local function on_move(data)
-    --         require("util").lsp.on_rename(data.source, data.destination)
-    --     end
-    --
-    --     local events = require("neo-tree.events")
-    --     opts.event_handlers = opts.event_handlers or {}
-    --     vim.list_extend(opts.event_handlers, {
-    --         { event = events.FILE_MOVED, handler = on_move },
-    --         { event = events.FILE_RENAMED, handler = on_move },
-    --     })
-    --     require("neo-tree").setup(opts)
-    --     vim.api.nvim_create_autocmd("TermClose", {
-    --         pattern = "*lazygit",
-    --         callback = function()
-    --             if package.loaded["neo-tree.sources.git_status"] then
-    --                 require("neo-tree.sources.git_status").refresh()
-    --             end
-    --         end,
-    --     })
-    -- end,
 }
