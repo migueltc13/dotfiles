6c98ea90c56b3d48fca2aa17d3de858e888a12c4
migueltc13 <migueltc13@gmail.com>
2024-01-04 13:09:38 +0000

    Remove util/ToggleTerm.lua

--- .config/nvim/lua/util/ToggleTerm.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-function M.open_vertical_term()
-    local w = vim.api.nvim_win_get_width(vim.api.nvim_get_current_win()) / 2
-    vim.cmd('ToggleTerm direction=vertical size=' .. tostring(w > 50 and w or 50))
-end
-
-return M
