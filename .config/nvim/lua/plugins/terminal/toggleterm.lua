4cfa2ecaa507e09c439c116dfd0c8c83335625fb
migueltc13 <migueltc13@gmail.com>
2024-01-04 13:11:00 +0000

    Update ToggleTerm config

--- .config/nvim/lua/plugins/terminal/toggleterm.lua
+++ .config/nvim/lua/plugins/terminal/toggleterm.lua
@@ -7,13 +7,31 @@ return {
         "ToggleTermSendVisualSelection",
         "ToggleTermSetName",
         "ToggleTermToggleAll",
+        "TermExec",
+        "Terminal",
     },
     version = "*",
     config = function()
-        require("toggleterm").setup{
+        local toggleterm = require("toggleterm")
+        toggleterm.setup{
+            size = function(term)
+                if term.direction == "vertical" then
+                    return vim.o.columns * 0.4
+                elseif term.direction == "horizontal" then
+                    return vim.opt.lines._value * 0.4
+                end
+            end,
             float_opts = {
                 border = "rounded",
             },
         }
+
+        -- Alias Terminal to ToggleTerm
+        local commandline = require("toggleterm.commandline")
+        vim.api.nvim_create_user_command(
+            "Terminal",
+            function(opts) toggleterm.toggle_command(opts.args, opts.count) end,
+            { count = true, complete = commandline.toggle_term_complete, nargs = "*" }
+        )
     end,
 }
