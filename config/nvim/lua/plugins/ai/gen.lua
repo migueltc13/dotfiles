c6ad89339d100ed45a37b4172bbbb125c4ec3bd4
migueltc13 <migueltc13@gmail.com>
2024-01-07 18:10:50 +0000

    Update gen config

--- config/nvim/lua/plugins/ai/gen.lua
+++ config/nvim/lua/plugins/ai/gen.lua
@@ -5,7 +5,7 @@ return {
     opts = {
         model = "mistral", -- The default model to use.
         display_mode = "split", -- The display mode. Can be "float" or "split".
-        show_prompt = false, -- Shows the Prompt submitted to Ollama.
+        show_prompt = true, -- Shows the Prompt submitted to Ollama.
         show_model = false, -- Displays which model you are using at the beginning of your chat session.
         no_auto_close = false, -- Never closes the window automatically.
         init = function(_) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
@@ -15,7 +15,7 @@ return {
         -- This can also be a lua function returning a command string, with options as the input parameter.
         -- The executed command must return a JSON object with { response, context }
         -- (context property is optional).
-        list_models = '<omitted lua function>', -- Retrieves a list of model names
+        -- list_models = '<omitted lua function>', -- Retrieves a list of model names
         debug = false -- Prints errors and the command which is run.
     }
 }
