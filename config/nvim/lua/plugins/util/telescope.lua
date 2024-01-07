710e02c49968417876e1631b8da6fc67097fd941
migueltc13 <migueltc13@gmail.com>
2024-01-07 18:29:19 +0000

    Update telescope config: Add treessiter as dependency + add insert mode mappings

--- config/nvim/lua/plugins/util/telescope.lua
+++ config/nvim/lua/plugins/util/telescope.lua
@@ -5,11 +5,30 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
+        "nvim-treesitter/nvim-treesitter",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
         local telescope = require("telescope")
-        telescope.setup()
+        local actions = require("telescope.actions")
+        telescope.setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        -- close with escape
+                        ["<esc>"] = actions.close,
+                        -- move up and down with Ctrl + k/j and mouse scroll
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<ScrollWheelDown>"] = actions.move_selection_next,
+                        ["<ScrollWheelUp>"] = actions.move_selection_previous,
+                        -- cycle history with Ctrl + n/p
+                        ["<C-n>"] = actions.cycle_history_prev,
+                        ["<C-p>"] = actions.cycle_history_next,
+                    },
+                },
+            }
+        })
         telescope.load_extension("fzf")
         telescope.load_extension("notify")
     end,
