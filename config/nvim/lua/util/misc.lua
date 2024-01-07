e365ae652304400f3f9f9543a210c4ed22647e37
migueltc13 <migueltc13@gmail.com>
2024-01-07 18:44:19 +0000

    Update selected all notification

--- config/nvim/lua/util/misc.lua
+++ config/nvim/lua/util/misc.lua
@@ -3,7 +3,7 @@ local M = {}
 -- Select all text in the buffer (Creates a mark 'A' that you can jump to)
 function M.select_all()
     vim.api.nvim_feedkeys('mA', 'n', true)
-    vim.notify("Created mark \'A\'", vim.log.levels.INFO, {title = "Select all text"})
+    vim.notify('Created mark "A"', vim.log.levels.INFO, {title = 'Select all text'})
     vim.api.nvim_feedkeys('ggVG', 'n', true)
 end
 
