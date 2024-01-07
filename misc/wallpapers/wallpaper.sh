3daa485f81f605499882e1e8966e36501460758c
migueltc13 <migueltc13@gmail.com>
2024-01-07 21:49:30 +0000

    Update misc/wallpapers directory

--- misc/wallpapers/wallpaper.sh
+++ misc/wallpapers/wallpaper.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 WALLPAPER_ROOT="${HOME}/Pictures/Wallpapers"
-WALLPAPER_DIR="${WALLPAPER_ROOT}/wallpapers"
+
+WALLPAPER_DIR="${WALLPAPER_ROOT}/catppuccin"
+# WALLPAPER_DIR="${WALLPAPER_ROOT}/defcon"
 
 NUMBER_OF_WALLPAPERS=$( ls ${WALLPAPER_DIR} | wc -w )
-RANDON_NUMBER=$(( ${RANDOM} % ${NUMBER_OF_WALLPAPERS} + 1 ))
+RANDON_NUMBER=$(( ${RANDOM} % ${NUMBER_OF_WALLPAPERS} ))
 ln -fs ${WALLPAPER_DIR}/wallpaper${RANDON_NUMBER}.png ${WALLPAPER_ROOT}/wallpaper.png
 echo $(ls -l ${WALLPAPER_ROOT}/wallpaper.png)
 exit 0
