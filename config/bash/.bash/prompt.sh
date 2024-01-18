131ef9c036f5eef1aa9d72856270f812a0012e25
flaviaraujo <flaviaraujo28@gmail.com>
2024-01-18 15:11:13 +0000

    Refactor bash prompt + add show host option

--- config/bash/.bash/prompt.sh
+++ config/bash/.bash/prompt.sh
@@ -43,41 +43,69 @@ if [ "$color_prompt" = yes ]; then
     _time="\[\e[3;38;5;246m\]"
     _path="\[\e[0;33m\]"
     _jobs="\[\e[0;35m\]"
+    _host_color="\[\e[0;35m\]"
+    _host_separator_color=${_time}
+    _git_color="\e[3;36m"
 
     # purple theme
     # _l="\[\e[0;35m\]"
     # _user="\[\e[1;35m\]"
-    # _path="\[\e[38;5;227m\]"
+    # _time="\[\e[3;38;5;246m\]"
+    # _path="\[\e[0;34m\]"
+    # _jobs="\[\e[0;36m\]"
+    # _host_color="\[\e[0;34m\]"
+    # _host_separator_color=${_time}
+    # _git_color="\e[3;92m"
+
+    # status colors
+    _success=${_l}
+    _stopped=${_jobs}
+    _interrupt="\[\e[0;33m\]"
+    _error="\[\e[0;31m\]"
+
+    # display host
+    _show_host=0
+    _host=""
+    if [ $_show_host -eq 1 ]; then
+        _host="${_host_separator_color}@${_host_color}\h"
+    fi
+
+    # git branch symbol
+    _git_symbol="⎇ "
+    # _git_symbol=" "
 
     # commands
     _time_cmd="${_time}\$(date +\"%H:%M:%S\")"
-    _git_cmd="\$(__git_ps1 \"\e[3;36m⎇  %s\")"
+    _git_cmd="\$(__git_ps1 \"\${_git_color}\${_git_symbol} %s\")"
     _jobs_cmd="${_jobs}\$(__stopped_jobs)"
 
     # dynamic prompt: error color + git branch + background jobs stack
     set_prompt_color() {
+        # prompt symbol color
         local status=$?
         case $status in
             0)   # success
-                PC="\[\e[0;32m\]";;
+                PC=${_success};;
             130) # SIGINT
-                PC="\[\e[0;33m\]";;
+                PC=${_interrupt};;
             147 | 148) # SIGTSTP or SIGINT
-                PC="\[\e[0;35m\]";;
-            *)   # any other error
-                PC="\[\e[0;31m\]";;
+                PC=${_stopped};;
+            *)   # error
+                PC=${_error};;
         esac
+        # git branch
         if [ -z "$(__git_ps1)" ]; then
             _gitl=""
         else
             _gitl="${_l}├─┤${_git_cmd}"
         fi
+        # background jobs stack
         if [ -z "$(__stopped_jobs)" ]; then
             _jobsl=""
         else
             _jobsl="${_l}├─┤${_jobs_cmd}"
         fi
-        PS1="${_l}╭──┤${_user}\u${_l}├─┤${_time_cmd}${_l}├─┤${_path}\w${_gitl}${_jobsl}${_l}│\n${_l}╰─${PC}▶\[\033[00m\] "
+        PS1="${_l}╭──┤${_user}\u${_host}${_l}├─┤${_time_cmd}${_l}├─┤${_path}\w${_gitl}${_jobsl}${_l}│\n${_l}╰─${PC}▶${_reset} "
         PS1="\[\e]0;\u@\h: \w   \a\]$PS1" # set window title
     }
     export PROMPT_COMMAND="set_prompt_color;$PROMPT_COMMAND"
