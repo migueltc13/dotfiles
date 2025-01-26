7131987feb22493276b432c818903c62e258a1c2
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2025-01-26 03:21:31 +0000

    Update make_executable function in util/misc.lua nvim config

--- config/nvim/lua/util/misc.lua
+++ config/nvim/lua/util/misc.lua
@@ -9,13 +9,41 @@ end
 
 -- Makes current file executable
 function M.make_executable()
+    -- Get full file path
     local filename = vim.fn.expand('%')
-    vim.fn.system({'chmod', '+x', filename})
 
+    -- Save the file
+    local status, err = pcall(vim.api.nvim_command, 'w')
+    if not status then
+        vim.notify(
+            'Failed to save \'' .. filename .. '\'' .. '\n' .. err,
+            vim.log.levels.ERROR,
+            {title = 'Failed to make file executable'}
+        )
+        return
+    end
+
+    -- Execute chmod +x on the file
+    local output = vim.fn.system({'chmod', '+x', filename})
+
+    -- Notify user of the result
     if vim.v.shell_error == 0 then
-        vim.notify(filename .. ' is now executable', vim.log.levels.INFO, {title = 'Success'})
+        vim.notify(
+            '\'' .. filename .. '\' is now executable',
+            vim.log.levels.INFO,
+            {title = 'Success'}
+        )
     else
-        vim.notify('Failed to make ' .. filename .. ' executable', vim.log.levels.ERROR, {title = 'Error'})
+        -- strip the last trailing newline from output
+        if output:sub(-1) == "\n" then
+            output = output:sub(1, -2)
+        end
+
+        vim.notify(
+            output,
+            vim.log.levels.ERROR,
+            {title = 'Failed to make \'' .. filename .. '\' executable' }
+        )
     end
 end
 
